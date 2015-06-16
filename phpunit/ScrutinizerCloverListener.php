03e33aa4b6555c8c2e2da1ed3b514e8205257db5
petertilsen <peter@uniplaces.com>
2015-06-16 18:00:25 +0100

    [RFC][NI] initial commit

--- phpunit/ScrutinizerCloverListener.php
+++ phpunit/ScrutinizerCloverListener.php
@@ -1,36 +1,43 @@
 <?php
 
-namespace phpunit;
+namespace Phpunit;
 
 use Symfony\Component\Process\Process;
+use Uniplaces\Library\Stringy\S;
 
 /**
- * Class ScrutinizerTestListener
- * @package phpunit
+ * Class ScrutinizerCloverListener
+ * @package Phpunit
  */
-class ScrutinizerTestListener extends \PHPUnit_Framework_BaseTestListener
+class ScrutinizerCloverListener extends \PHPUnit_Framework_BaseTestListener
 {
 
     const OCULAR_PHAR = 'ocular.phar';
     const OCULAR_HOST = 'https://scrutinizer-ci.com';
-    const INSTALL_DIR = 'bin';
+    const INSTALL_DIR = '/tmp';
     const SCRUTINIZER_API_TOKEN = '7c8ad0ef7f0a42323cee76eaaf9da7a7339acd24b23ccd09a1dbe2f1c5fbcaa9';
 
     /**
      * @param \PHPUnit_Framework_TestSuite $suite
      * @return bool
-     * @throws \HttpException
+     * @throws \Exception
      */
     public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
-        if (!is_file(self::INSTALL_DIR . '/' . self::OCULAR_PHAR )) {
+        $file = S::render('{installDir}/{ocularPhar}',
+            [
+                'installDir' => self::INSTALL_DIR,
+                'ocularPhar' => self::OCULAR_PHAR
+            ]);
+
+        if (!is_file($file)) {
             if (!$this->downloadOcular()) {
-                throw new \HttpException('Ocular could not be downloaded');
+                throw new \Exception('Ocular could not be downloaded');
             }
         }
 
         if (!$this->uploadCloverReport()) {
-            throw new \HttpException('Clover report could not be uploaded');
+            throw new \Exception('Clover report could not be uploaded');
         }
 
         return true;
@@ -41,7 +48,16 @@ class ScrutinizerTestListener extends \PHPUnit_Framework_BaseTestListener
      */
     protected function uploadCloverReport()
     {
-        return $this->process('php ocular.phar code-coverage:upload --access-token="' . self::SCRUTINIZER_API_TOKEN . '" --format=php-clover docs/phpunit/clover.xml');
+        $process = S::render(
+            'php {installDir}/{ocularPhar} code-coverage:upload --access-token="{scrutinizerApiToken}" --format=php-clover {clover}',
+            [
+                'ocularPhar' => self::OCULAR_PHAR,
+                'installDir' => self::INSTALL_DIR,
+                'scrutinizerApiToken' => self::SCRUTINIZER_API_TOKEN,
+                'clover' => __DIR__ . '/../../docs/phpunit/clover.xml'
+            ]);
+
+        return $this->process($process);
     }
 
     /**
@@ -49,7 +65,15 @@ class ScrutinizerTestListener extends \PHPUnit_Framework_BaseTestListener
      */
     protected function downloadOcular()
     {
-        return $this->process('wget ' . self::OCULAR_HOST . '/' . self::OCULAR_PHAR . ' ' . self::INSTALL_DIR . '/' . self::OCULAR_PHAR);
+        $process = S::render(
+            'wget {ocularHost}/{ocularPhar} && mv {ocularPhar} {installDir}/{ocularPhar}',
+            [
+                'ocularHost' => self::OCULAR_HOST,
+                'ocularPhar' => self::OCULAR_PHAR,
+                'installDir' => self::INSTALL_DIR
+            ]);
+
+        return $this->process($process);
     }
 
     /**
@@ -58,12 +82,21 @@ class ScrutinizerTestListener extends \PHPUnit_Framework_BaseTestListener
      */
     protected function process($process)
     {
-        $process = new Process($process);
+        $process = $this->getProcess($process);
         $process->run();
 
         if ($process->isSuccessful()) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * @param $process
+     * @return Process
+     */
+    protected function getProcess($process)
+    {
+        return new Process($process);
     }
 }
